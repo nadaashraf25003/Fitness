f9a1435379782456ae1932ac7d592cfe43929996
MarinaShenoudaFathy <marenashnoda1@gmail.com>
2026-09-13 23:11:45 +0300

    Create SubscriptionsPage.tsx

---
+++ src/Views/subscriptions/SubscriptionsPage.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import { PlaceholderPage } from '../shared/PlaceholderPage';
+export const SubscriptionsPage: React.FC = () => <PlaceholderPage title="Members & Plans" description="Manage members and their membership plans." />;
