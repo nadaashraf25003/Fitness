ac931242600e6e5bbc1f7221eb708fcb010544af
MarinaShenoudaFathy <marenashnoda1@gmail.com>
2026-09-13 23:11:50 +0300

    Create SubscriptionRequestsInbox.tsx

---
+++ src/Views/subscriptions/SubscriptionRequestsInbox.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import { PlaceholderPage } from '../shared/PlaceholderPage';
+export const SubscriptionRequestsInbox: React.FC = () => <PlaceholderPage title="Subscription Requests" description="Review pending membership applications." />;
